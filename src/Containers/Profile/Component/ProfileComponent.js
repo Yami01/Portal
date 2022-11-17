97d407cd1d1c98d8d24152e342c2ea6a9f71c17e
techvify_khanhtq <61421195+tranquockhanh0506@users.noreply.github.com>
2022-11-17 09:36:14 +0700

    fix conflict

--- src/Containers/Profile/Component/ProfileComponent.js
+++ src/Containers/Profile/Component/ProfileComponent.js
@@ -12,27 +12,12 @@ import { validatorLogin } from '@/validation'
 import Button from '@/Components/Button'
 import type { PropsType } from "@/Containers/Profile/Types"
 
+
 const ProfileComponent: AbstractComponent<PropsType> = memo((props: PropsType): Node => {
   const {primaryButtonPress, secondaryButtonPress} = props;
   const { Layout, Fonts } = useTheme()
-import React, { useEffect, useState } from "react"
-import { Text, TouchableOpacity, View } from "react-native"
-import { useTranslation } from "react-i18next"
-import { useTheme } from "@/Hooks"
-import ProfileComponentStyles from "../Styles"
-import { useForm } from "react-hook-form"
-import ScreenContainer from "@/Components/ScreenContainer"
-import ScreenLayout from "@/Components/ScreenLayout"
-import Input from "@/Components/Input"
-import * as yup from "yup"
-import { yupResolver } from "@hookform/resolvers/yup"
-import { validatorLogin } from "@/validation"
-
-
-const ProfileComponent = () => {
-  const { Layout, Fonts } = useTheme();
   const { t } = useTranslation()
-  const [isDisableLoginBTN, setIsDisableLoginBTN] = useState(true)
+  // const [isDisableLoginBTN, setIsDisableLoginBTN] = useState(true);
 
   const {
     control,
@@ -42,8 +27,8 @@ const ProfileComponent = () => {
   } = useForm({
     resolver: yupResolver(validatorLogin),
     defaultValues: {
-      userName: "",
-      password: "",
+      userName: '',
+      password: '',
     },
     mode: 'onBlur',
     reValidateMode: 'onChange',
@@ -52,16 +37,16 @@ const ProfileComponent = () => {
   console.log(control);
   return (
     <ScreenContainer backgroundType={"image"} backgroundImage={require("@/Assets/Images/bgr_login.png")}>
-      <View style={{ height: "25%" }} />
+      <View style={{ height: '25%' }} />
       <ScreenLayout scrollable style={[Layout.fill]}>
         <View style={[ProfileComponentStyles.container]}>
           <Text style={[ProfileComponentStyles.tileStyle]}>Đăng nhập tài khoản</Text>
           <Input label={''} fieldName={'userName'} control={control} placeholder={'Tên đăng nhập'} error={errors?.userName?.message} />
           <View style={{ height: 10 }} />
           <Input label={''} fieldName={'password'} control={control} placeholder={'Mật khẩu'} error={errors?.password?.message} />
-          <View style={{ flexDirection: "row", justifyContent: 'space-between', marginTop: 20 }}>
-            <Button label = {"Quay lai"} onPress={handleSubmit(secondaryButtonPress)}></Button>
-            <Button label = {"Quay lai"} onPress={() => primaryButtonPress(1)}></Button>
+          <View style={{ flexDirection: "row", justifyContent: 'space-between', marginTop: 20 ,width: '100%'}}>
+            <Button label = {"Quay lai"} onPress={handleSubmit(secondaryButtonPress)} btnStyle= {{width: '50%',}}></Button>
+            <Button label = {"Quay lai"} onPress={() => primaryButtonPress(1)} btnStyle= {{width: '50%',}}></Button>
           </View>
         </View>
       </ScreenLayout>
